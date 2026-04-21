e5feacd86c9faac173e8f393bb87517c02a3247e
Tilan Wishwajith <tilanwishwajith@gmail.com>
2026-04-21 23:41:24 +0530

    Added Canvas for Drawing

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from PIL import Image
 with st.sidebar:
     st.header("Instructions")
     st.markdown("1. Draw a digit clearly\n2. Click Predict\n3. See the magic!")
+
+from streamlit_drawable_canvas import st_canvas
+canvas_result = st_canvas(
+    stroke_width=18, 
+    stroke_color="#FFFFFF", 
+    background_color="#000000", 
+    height=280, width=280, 
+    drawing_mode="freedraw"
+)

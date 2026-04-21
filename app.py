0b4b713e5921bfe95cdbc425abb0818ffb8e8c26
Tilan Wishwajith <tilanwishwajith@gmail.com>
2026-04-21 23:40:43 +0530

    Sidebar Instructions

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ st.title("Digit Recognizer AI")
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+with st.sidebar:
+    st.header("Instructions")
+    st.markdown("1. Draw a digit clearly\n2. Click Predict\n3. See the magic!")
